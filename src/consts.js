d3e756f035c25d31b6bd1ededeed93dc82523ab8
IATOW <584130248@qq.com>
2020-02-17 20:34:19 +0800

    worm

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
 export default {
-    url: process.env.NODE_ENV === "development" ? "http://localhost:8080/" : "http://118.178.121.25/8080/",
-    wsUrl: process.env.NODE_ENV === "development" ? "ws://localhost:8080/" : "ws://118.178.121.25/8080/",
+    url: process.env.NODE_ENV === "development" ? "http://localhost:8080/" : "http://118.178.121.25:8080/",
+    wsUrl: process.env.NODE_ENV === "development" ? "ws://localhost:8080/" : "ws://118.178.121.25:8080/",
 }
